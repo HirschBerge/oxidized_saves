f25bbc42bd7d4e71ba4958d33016e7efaf073073
HirschBerge <hirschy.kirkwood@proton.me>
2024-04-13 00:21:11 -0400

    Small QoL improvements on steam stuff

--- src/config/save.rs
+++ src/config/save.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 #[allow(dead_code)]
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Save {
-    pub count: u8,
+    pub count: u16,
     pub backup_path: PathBuf,
     pub production_path: PathBuf,
     pub parent_game: String,
