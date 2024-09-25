0145d90d0d82dd6ab189ecb51738947d0b40bc0b
HirschBerge <hirschy.kirkwood@proton.me>
2024-09-25 19:58:41 -0400

    exports discovered steam games to file
    location being $HOME/.config/oxi/steamgame.json

--- src/main.rs
+++ src/main.rs
@@ -57,5 +57,6 @@ fn main() {
 
     // After modifications, write the `games` vector back to the configuration file
     write_conf(games, &game_conf_path);
-    discover_steamgames();
+    let discovered_games = discover_steamgames();
+    write_conf(discovered_games, home_dir.join(".config/oxi/steam_games.json").as_path())
 }
